a695584a8d6696a869018a3ee823e3ec8fda67a9
pointandlaugheveryone <veronika.havlova@mensagymnazium.cz>
2026-01-24 13:36:40 +0100

    add client

--- 5_AoC/Program.cs
+++ 5_AoC/Program.cs
@@ -1,19 +1,34 @@
-﻿void D1(){
-	using var sr = new StreamReader(("input.txt"));
+﻿using var sr = new StreamReader(("input.txt"));
 
+void D1(){
 	int zcnt = 0, t = 50;
 	var lines = File.ReadAllLines("input.txt");
 	foreach (var l in lines)
 	{
-		if (!int.TryParse(l.AsSpan(1), out var value)) continue;
+		if (!int.TryParse(l.AsSpan(1), out var val)) continue;
 
-		t += ( l[0] == 'R') ? value : -value;
+		t += ( l[0] == 'R') ? val : -val;
 		if (t%100==0) zcnt++;
 	}
 	Console.WriteLine($"{zcnt}");
 }
 
-void D2()
+void D1p2()
 {
+	int zcnt = 0, t = 50;
+	var lines = File.ReadAllLines("input.txt");
+	foreach (var l in lines)
+	{
+		if (!int.TryParse(l.AsSpan(1), out var val)) continue;
+		if (l[0] == 'R')
+		{
+			t += val;
+			zcnt += t / 100;
+			t %= 100;
+		}
+		else
+		{
 
+		}
+	}
 }
